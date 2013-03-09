f27e63998386f67761d7c214b1f25288ec04228f
B96152 <PaulHH03191975@fullsail.edu>
2013-03-08 21:05:19 -0600

    Added new procedure and boolean function to code.

--- holmes_paul_assignment2.js
+++ holmes_paul_assignment2.js
@@ -11,129 +11,68 @@
 
 var surfWaves = true,
 	waterTemp = 78,
-	airTemp = 80
+	airTemp = 80,
 	sunShine = true,
 	sunScreenOn = "I will apply my sunscreen",
-	sunScreenOff = "I will not apply my sunsceen"
-	sunBright = "\"Lots of Sunshine!\""
+	sunScreenOff = "I will not apply my sunsceen",
+	sunBright = "\"Lots of Sunshine!\"",
+	var temp = comfortableTemp(70,80);
 	
 ;
 
+
+
 // My Procedure
 
-if (surfWaves === true){
-// True
-	console.log("Since there are waves I want to go surfing.");
-	console.log("I will take my short board.");
-	// Output
-	
-} else {
-// False
-	console.log("Since there are not any waves I will go down to the Gulf of Mexico and wait for waves!");
-	console.log("I will take my long board.");
-	// Output
-	
-}; // Back
+var waveForm = function(waveHeight) {
 
-
-
-
-
-
-
-
-
-
-var getArea = function () {
-	console.log("getArea fired!");
+	if (waveHeight > 1){
+	// True
+		console.log("The waves are " + waveHeight + "' high!");
+		console.log("Since there are waves I want to go surfing.");
+		console.log("I will take my short board.");
+		// Output
+		
+	} else {
+	// False
+		console.log("Since there are not any waves I will go down to the Gulf of Mexico and wait for waves!");
+		console.log("I will take my long board.");
+		// Output
+		
+	};
 };
 
-console.log("I'm technically first.");
-
-getArea();
-
+waveForm(3)
+// Back
 
 
+// My Boolean Function
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*
-
-// My Outputs
-
-console.log("The surf reports that it is " + surfWaves + " that there may be waves.");
-console.log("The water temperature is " + waterTemp + ".");
-console.log("The air temperature is " + airTemp + ".");
-console.log("The surf report claims there is sun, when I look outside I can tell that this is " + sunShine + ".");
-console.log("It may be that if there is sun " + sunScreenOn + ".");
-console.log("It may be that there will be " + sunBright + ".");
-console.log("It may be that if there is no sun so " + sunScreenOff + ".");
-
-
-
-
-
-// Boolean Conditional
-
-if (surfWaves === true){
-// True
-	console.log("Since there are waves I want to go surfing.");
-	console.log("I will take my short board.");
-			
-		//	Number Conditional
-
-		if (((waterTemp + airTemp) / 2) >= 70){
-		// True
-			console.log("The average temperature is " + ((waterTemp + airTemp) / 2) + ".");
-			// Math
-			console.log("The water is warm enough to not wear a wetsuit.");
-			console.log("Time to drive down to the Gulf of Mexico!");
-			// Output A
-		} else {
-		// False
-			console.log("The water is not warm enough to wear just swim trunks.");
-			console.log("Time to drive down to the Gulf of Mexico!");
-			// Output B
-		}
-} else {
-// False
-	console.log("Since there are not any waves I will go down to the Gulf of Mexico and wait for waves!");
-	console.log("I will take my long board.");
+var comfortableTemp = function(waterTemp,airTemp) {
+	var aveTemp = ((waterTemp + airTemp) / 2);
+	
+	if (aveTemp >= 70) {
+	// True
+		console.log("The average temperature is " + aveTemp + "F. This is good for surfing!");
+		// Output
 		
-		// String Conditional
+		return true;
+		// Return Boolean True
 		
-		if (sunShine === true){
-		// True
-			console.log("It looks like there is " + sunBright + ", so " + sunScreenOn + ".");
-			// String Concatenation
-			console.log("Time to drive down to the Gulf of Mexico!");
-			// Output C
-		} else {
-		// False
-			console.log("It looks like the sun is not out today so " + sunScreenOff + ".");
-			console.log("Time to drive down to the Gulf of Mexico!");
-			// Output D
+	} else {
+	// False
+		console.log("The average temperature is " + aveTemp + "F. This is a little chilly to surf. May need to wear my wetsuit!");
+		// Output
+		
+		return false
+		// Return Boolean False
 	};
 	
-}; // Finish
+};
 
-*/
+
+
+
+
+
+
